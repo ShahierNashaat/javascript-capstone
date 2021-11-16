70ff214bacf38675d9d4a37b4a47d33fdfd1a6fb
Shahier Nashaat <shahiern@yahoo.com>
2021-11-16 10:41:03 +0200

    Design the header

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,10 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
     ],
   },
 };
